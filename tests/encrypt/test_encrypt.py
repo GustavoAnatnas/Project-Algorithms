020e70f2fefeff964449f5892a7813c607684ccd
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-03-01 13:06:17 -0300

    Implementa testes de Ciptografia de Inverões

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message("message", "")
+        encrypt_message(1, 2)
+        encrypt_message("true", "true")
+    assert encrypt_message("testes", -1) == "setset"
+    assert encrypt_message("testes", 3) == "set_set"
+    assert encrypt_message("testes", 4) == "se_tset"
+    
