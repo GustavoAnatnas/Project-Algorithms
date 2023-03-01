043576e040f3d96bb43197801dbf092fd8db5d43
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-03-01 12:10:42 -0300

    Algoritmo de Busca. Função "study_schedule"

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                count += 1
+    except (TypeError, ValueError):
+        return None
+
+    return count
